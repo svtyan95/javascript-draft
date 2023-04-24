10ce09d0f38750427771288b97dc0db2921dc748
Svyatoslav Tymchyna <svtyan95@gmail.com>
2023-04-23 22:32:41 -0500

    jjj

--- module-two.js
+++ module-two.js
@@ -145,17 +145,17 @@
 // withdraw(500, 300); // "Недостатньо коштів на рахунку"
 // withdraw(100, 300); // "Операція зняття коштів проведена успішно"
 
-function filterArray(numbers, value) {
-   // Change code below this line
-let newArray = [];
+// function filterArray(numbers, value) {
+//    // Change code below this line
+// let newArray = [];
 
-  for (let number of numbers) {
+//   for (let number of numbers) {
 
-    if (number > value) {
-      newArray.push(number)
-    }
-  }
+//     if (number > value) {
+//       newArray.push(number)
+//     }
+//   }
 
-  // Change code above this line
-  return newArray
-}
+//   // Change code above this line
+//   return newArray
+// }
